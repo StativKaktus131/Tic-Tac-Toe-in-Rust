22b5b5a28fa5d7049fec3cd1ff999ac9307eb64e
StativKaktus131 <152688915+StativKaktus131@users.noreply.github.com>
2024-07-12 02:38:23 +0200

    Deleted target directory

--- target/debug/deps/tryout_app.d
+++
@@ -1,5 +0,0 @@
-D:\Programming\Rust\Projects\Tryout\tryout_app\target\debug\deps\tryout_app.exe: src/main.rs
-
-D:\Programming\Rust\Projects\Tryout\tryout_app\target\debug\deps\tryout_app.d: src/main.rs
-
-src/main.rs:
--- target/debug/tryout_app.d
+++
@@ -1,1 +0,0 @@
-D:\Programming\Rust\Projects\Tryout\tryout_app\target\debug\tryout_app.exe: D:\Programming\Rust\Projects\Tryout\tryout_app\src\main.rs
